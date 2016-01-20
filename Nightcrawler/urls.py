042536d6f8aebbfce898e49379f70d1a5673d79b
qiubix <qiubix@gmail.com>
2016-01-20 13:59:21 +0100

    Add polls app

--- Nightcrawler/urls.py
+++ Nightcrawler/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^tenders/', include('tenders.urls')),
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
