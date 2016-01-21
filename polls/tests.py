88f8d592e524c2f8658086f18f480247d047711f
qiubix <qiubix@gmail.com>
2016-01-21 02:32:18 +0100

    Test detail view

--- polls/tests.py
+++ polls/tests.py
@@ -8,7 +8,6 @@ from .models import Question
 
 
 class QuestionMethodTests(TestCase):
-
     def test_was_published_recently_with_old_question(self):
         """
         was_published_recently() should return False for questions whose
@@ -35,6 +34,7 @@ class QuestionMethodTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertEqual(future_question.was_published_recently(), False)
+
 
 def create_question(question_text, days):
     """
@@ -105,3 +105,27 @@ class QuestionViewTests(TestCase):
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
 
+
+class QuestionIndexDetailTests(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future should
+        return a 404 not found.
+        """
+        future_question = create_question(question_text='Future question.',
+                                          days=5)
+        response = self.client.get(reverse('polls:detail',
+                                           args=(future_question.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_a_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past should
+        display the question's text.
+        """
+        past_question = create_question(question_text='Past Question.',
+                                        days=-5)
+        response = self.client.get(reverse('polls:detail',
+                                           args=(past_question.id,)))
+        self.assertContains(response, past_question.question_text,
+                            status_code=200)
