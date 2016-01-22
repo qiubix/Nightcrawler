c9e108a7482252d060885cefee467bb19786c7b7
qiubix <qiubix@gmail.com>
2016-01-22 23:30:24 +0100

    Create django models for Contractors and Procurers

--- Nightcrawler/settings.py
+++ Nightcrawler/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tenders.apps.TendersConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- tenders/models.py
+++ tenders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Procurer(models.Model):
+    company_name = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+
+
+class Contractor(models.Model):
+    company_name = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
