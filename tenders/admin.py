470085484e8fed44d966b2740e5da22ce636b8b8
qiubix <qiubix@gmail.com>
2016-01-23 12:59:18 +0100

    Register ProcurerAdmin site

--- tenders/admin.py
+++ tenders/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Tender, Contractor, Procurer
+
+
+class ProcurerInline(admin.TabularInline):
+    model = Procurer
+    extra = 3
+
+
+class ProcurerAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['company_name']}),
+        (None, {'fields': ['city']}),
+        (None, {'fields': ['address']}),
+        # ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    list_display = ('company_name', 'city', 'address')
+    # list_filter = ['city']
+    search_fields = ['company_name']
+
+
+admin.site.register(Procurer, ProcurerAdmin)
