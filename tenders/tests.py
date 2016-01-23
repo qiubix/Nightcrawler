f514641d72c6032c21fa466a70af1bd05f98287f
qiubix <qiubix@gmail.com>
2016-01-23 13:45:42 +0100

    Add tests for displaying procurers

--- tenders/tests.py
+++ tenders/tests.py
@@ -1,6 +1,7 @@
 from hamcrest import *
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from .models import Procurer
 
 
 # Create your tests here.
@@ -17,9 +18,42 @@ class IndexViewTests(TestCase):
                             "This site displays map of contractors and procurers connected by executed contracts.")
 
 
+def createProcurer(companyName, city, address):
+    return Procurer.objects.create(company_name=companyName, city=city, address=address)
+
+
 class ProcurersViewTests(TestCase):
     def test_procurer_view_with_no_data(self):
         response = self.client.get(reverse('tenders:procurers'))
+
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No procurers are available.")
         self.assertQuerysetEqual(response.context['all_procurers_list'], [])
+
+    def test_should_display_one_procurer_in_table(self):
+        createProcurer('Company name', 'Some City', 'Full address')
+
+        response = self.client.get(reverse('tenders:procurers'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['all_procurers_list'], ['<Procurer: Company name>'])
+        self.assertContains(response, '<table style=\"width:100%\">')
+        self.assertContains(response, '<td><p>Company name</p></td>')
+        self.assertContains(response, '<td><p>Some City</p></td>')
+        self.assertContains(response, '<td><p>Full address</p></td>')
+
+    def test_should_display_multiple_procurers(self):
+        createProcurer('First', 'City 1', 'Street 1')
+        createProcurer('Second', 'City 2', 'Street 2')
+        createProcurer('Third', 'City 3', 'Street 3')
+
+        response = self.client.get(reverse('tenders:procurers'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['all_procurers_list'],
+                                 ['<Procurer: First>', '<Procurer: Second>', '<Procurer: Third>'], ordered=False)
+                                 # ['<Procurer: First>'])
+        self.assertContains(response, '<table style=\"width:100%\">')
+        self.assertContains(response, '<td><p>First</p></td>')
+        self.assertContains(response, '<td><p>City 2</p></td>')
+        self.assertContains(response, '<td><p>Street 3</p></td>')
