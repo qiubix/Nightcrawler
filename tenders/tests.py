670502d4c73c81f3b395e5078594dc52d78194f3
qiubix <qiubix@gmail.com>
2016-01-23 14:09:20 +0100

    Enhance procurers table style

--- tenders/tests.py
+++ tenders/tests.py
@@ -30,6 +30,18 @@ class ProcurersViewTests(TestCase):
         self.assertContains(response, "No procurers are available.")
         self.assertQuerysetEqual(response.context['all_procurers_list'], [])
 
+    def test_should_display_table_when_procurers_exist(self):
+        createProcurer('Company name', 'Some City', 'Full address')
+
+        response = self.client.get(reverse('tenders:procurers'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<table>')
+        self.assertContains(response, '<th>ID</th>')
+        self.assertContains(response, '<th>Company name</th>')
+        self.assertContains(response, '<th>City</th>')
+        self.assertContains(response, '<th>Full address</th>')
+
     def test_should_display_one_procurer_in_table(self):
         createProcurer('Company name', 'Some City', 'Full address')
 
@@ -37,10 +49,9 @@ class ProcurersViewTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertQuerysetEqual(response.context['all_procurers_list'], ['<Procurer: Company name>'])
-        self.assertContains(response, '<table style=\"width:100%\">')
-        self.assertContains(response, '<td><p>Company name</p></td>')
-        self.assertContains(response, '<td><p>Some City</p></td>')
-        self.assertContains(response, '<td><p>Full address</p></td>')
+        self.assertContains(response, '<td>Company name</td>')
+        self.assertContains(response, '<td>Some City</td>')
+        self.assertContains(response, '<td>Full address</td>')
 
     def test_should_display_multiple_procurers(self):
         createProcurer('First', 'City 1', 'Street 1')
@@ -52,8 +63,6 @@ class ProcurersViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertQuerysetEqual(response.context['all_procurers_list'],
                                  ['<Procurer: First>', '<Procurer: Second>', '<Procurer: Third>'], ordered=False)
-                                 # ['<Procurer: First>'])
-        self.assertContains(response, '<table style=\"width:100%\">')
-        self.assertContains(response, '<td><p>First</p></td>')
-        self.assertContains(response, '<td><p>City 2</p></td>')
-        self.assertContains(response, '<td><p>Street 3</p></td>')
+        self.assertContains(response, '<td>First</td>')
+        self.assertContains(response, '<td>City 2</td>')
+        self.assertContains(response, '<td>Street 3</td>')
