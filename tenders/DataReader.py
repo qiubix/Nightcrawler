6ed9664bc4bd85acac33b982465648e9e62e0af1
qiubix <qiubix@gmail.com>
2016-01-28 17:19:18 +0100

    Read procurer's name from file

--- tenders/DataReader.py
+++ tenders/DataReader.py
@@ -1,3 +1,6 @@
+from xml.dom.minidom import parse
+
+
 class ContractorData:
     pass
 
@@ -12,18 +15,27 @@ class DataReader:
     def __init__(self):
         self.procurers = []
         self.contractors = []
+        self.document = None
 
     def load(self, fileName):
         self.contractors.append(ContractorData())
 
-        procurer = ProcurerData()
-        procurer.company_name = 'Oddział Specjalny Żandarmerii Wojskowej'
-        procurer.city = 'Mińsk Mazowiecki'
-        second = ProcurerData()
-        second.company_name = 'Centrum Onkologii Instytut im. Marii Skłodowskiej-Curie'
-        second.city = 'Warszawa'
-        self.procurers.append(procurer)
-        self.procurers.append(second)
+        with open(fileName, 'r') as file:
+            self.document = parse(file)
+
+        ogloszenia = self.document.getElementsByTagName('ogloszenie')
+        for ogloszenie in ogloszenia:
+            procurer = ProcurerData()
+            childNodes = ogloszenie.childNodes
+            for childNode in childNodes:
+                if childNode.nodeName == 'nazwa':
+                    companyName = childNode.firstChild.nodeValue.rstrip()
+                    procurer.company_name = companyName
+                if childNode.nodeName == 'miejscowosc':
+                    city = childNode.firstChild.nodeValue.rstrip()
+                    procurer.city = city
+
+            self.procurers.append(procurer)
 
     def getContractors(self):
         return self.contractors
