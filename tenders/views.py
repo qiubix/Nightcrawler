a0b12865fd67d8196c38975da987d1beece35976
qiubix <qiubix@gmail.com>
2016-01-31 17:26:31 +0100

    Display procurer on map

--- tenders/views.py
+++ tenders/views.py
@@ -1,16 +1,21 @@
-from django.http import HttpResponse
+import googlemaps
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.views import generic
 
+from Nightcrawler.settings import GOOGLE_API_SECRET_KEY
+from tenders.CompanyLocator import CompanyLocator
 from tenders.DataReader import DataReader
 from .models import Procurer, Tender, Contractor
 
 
 class IndexView(generic.ListView):
     template_name = 'tenders/index.html'
-    context_object_name = 'tenders'
+    context_object_name = 'procurer'
 
     def get_queryset(self):
-        return Tender.objects.all()
+        return Procurer.objects.all().get(pk=2)
 
 
 class ProcurersView(generic.ListView):
@@ -39,6 +44,21 @@ def importView(request):
         Procurer.objects.create(company_name=procurer.company_name, city=procurer.city, address=procurer.full_address)
 
     for contractor in contractors:
-        Contractor.objects.create(company_name=contractor.company_name, city=contractor.city, address=contractor.full_address)
+        Contractor.objects.create(company_name=contractor.company_name, city=contractor.city,
+                                  address=contractor.full_address)
 
     return HttpResponse('<h2>Data imported!</h2>')
+
+
+def displayProcurers(request):
+    procurer = get_object_or_404(Procurer, pk=2)
+    if procurer.latitude is None or procurer.longitude is None:
+        gmapsClient = googlemaps.Client(key=GOOGLE_API_SECRET_KEY)
+        locator = CompanyLocator(gmapsClient)
+        location = locator.getLocation(procurer.address)
+        procurer.latitude = location.get('lat')
+        procurer.longitude = location.get('lng')
+        procurer.save()
+
+    return HttpResponseRedirect(reverse('tenders:index'))
+
